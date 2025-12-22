8765a81d4a3f9fc86244fed6424fd4a35cca6245
naveid12 <muhammadnaveed8826@gmail.com>
2025-12-22 13:04:19 +0500

    two file hase tracked and es 6 modules

---
+++ app.js
@@ -0,0 +1,12 @@
+//const math = require("./math"); // comman js ma 
+
+//import mul from "./math.js";
+
+
+import { mul,div } from "./math.js"; //  name ka throwo
+
+// console.log(math.add(5,10));
+// console.log(math.sub(5,10));
+console.log(div(5,10));
+console.log(mul(10,10));
+// console.log(math.PI);
---
+++ math.js
@@ -0,0 +1,19 @@
+const add = (a,b) =>{
+    return a+b;
+};
+
+const sub = (a ,b) =>{
+    return a-b;
+};
+
+ export const div = (a,b) =>{
+    return a/b;
+};
+
+ export const mul = (a,b) =>{
+    return a*b;
+};
+
+const PI = 3.214;
+
+// export default mul; defult es 6 
